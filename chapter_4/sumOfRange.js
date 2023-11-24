6a0a9bfdee3f43cfb2693c7f3a9f9514ac37c711
nbarnabee <nicole.barnabee@gmail.com>
2023-11-24 20:18:58 +0100

    Commit problem and solution

---
+++ chapter_4/sumOfRange.js
@@ -0,0 +1,34 @@
+/*  
+Write a function that takes two arguments, start and end, and returns an array containing all the numbers from start to end, inclusive.
+
+Then write a sum function that takes an array of numbers and returns the sum of those numbers.
+
+Then modify the range function to take an option third argument, a "step".  Make sure it also works with negative step values.
+
+*/
+
+function range(start, end) {
+  const result = [];
+  for (i = start; i <= end; i++) {
+    result.push(i);
+  }
+  return result;
+}
+
+const sum = (arr) => arr.reduce((accum, curr) => accum + curr);
+
+function rangeRedux(start, end, step = 1) {
+  const result = [];
+  if (start <= end) {
+    for (i = start; i <= end; i += step) {
+      result.push(i);
+    }
+  } else {
+    for (i = start; i >= end; i += step) {
+      result.push(i);
+    }
+  }
+  return result;
+}
+
+// This feels very repetitive, but my head's not in it tonight.
